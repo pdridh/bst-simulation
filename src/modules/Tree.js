727ad40a93c017ca7f587ceb3150fd41f54eafdd
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-10-16 13:31:02 +0545

    Create Tree module that can build BSTs

---
+++ src/modules/Tree.js
@@ -0,0 +1,35 @@
+import Node from "./Node";
+
+class Tree {
+  constructor(data) {
+    this.root = null;
+    if (data) {
+      this.build(data);
+    }
+  }
+
+  #filterData(data) {
+    const filtered = data.filter((val, ind) => data.indexOf(val) === ind);
+    filtered.sort((a, b) => a - b);
+    return filtered;
+  }
+
+  build(data) {
+    data = this.#filterData(data);
+    this.root = this.buildTree(data);
+  }
+
+  buildTree(data, start = 0, end = data.length - 1) {
+    if (start > end) return null;
+
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(data[mid]);
+
+    root.left = this.buildTree(data, start, mid - 1);
+    root.right = this.buildTree(data, mid + 1, end);
+
+    return root;
+  }
+}
+
+export default Tree;
