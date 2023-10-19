76bbd7baaeab7be18ed77b110b1714943a13e66a
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-10-19 17:34:24 +0545

    Add movement to Camera

--- src/modules/Camera.js
+++ src/modules/Camera.js
@@ -27,6 +27,8 @@ class Camera {
       this.worldBounds.maxX > this.canvas.width ||
       this.worldBounds.minY < 0 ||
       this.worldBounds.maxY > this.canvas.width;
+    this.speedX = (1 / 100) * worldBounds.maxX;
+    this.speedY = (1 / 100) * worldBounds.maxY;
   }
 
   // Calibrates the canvas according to the target position
@@ -42,6 +44,39 @@ class Camera {
       this.worldBounds.maxY - this.canvas.height
     );
   }
+
+  update() {
+    if (EventHandler.isKeyPressed("ArrowRight")) {
+      this.targetX += this.speedX;
+    }
+
+    if (EventHandler.isKeyPressed("ArrowLeft")) {
+      this.targetX -= this.speedX;
+    }
+
+    if (EventHandler.isKeyPressed("ArrowUp")) {
+      this.targetY -= this.speedY;
+    }
+
+    if (EventHandler.isKeyPressed("ArrowDown")) {
+      this.targetY += this.speedY;
+    }
+
+    if (this.targetX - this.canvas.width / 2 < this.worldBounds.minX) {
+      this.targetX = this.worldBounds.minX + this.canvas.width / 2;
+    } else if (this.targetX + this.canvas.width / 2 > this.worldBounds.maxX) {
+      this.targetX = this.worldBounds.maxX - this.canvas.width / 2;
+    }
+
+    if (this.targetY - this.canvas.height / 2 < 0) {
+      this.targetY = this.canvas.height / 2;
+    } else if (this.targetY + this.canvas.height / 2 > this.worldBounds.maxY) {
+      this.targetY = this.worldBounds.maxY - this.canvas.height / 2;
+    }
+
+    this.calibrate();
+  }
+
 }
 
 export default Camera;
